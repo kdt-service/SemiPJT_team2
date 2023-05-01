7d8b6f31f6acfa5f4517d4f698536f8e5afae6ba
daeun-moon <anna202204@naver.com>
2023-05-01 16:27:58 +0900

    arrange all files

---
+++ 3_1 job_keyword (토큰화포함).py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from konlpy.tag import Mecab
+import warnings
+warnings.filterwarnings('ignore')
+
+class JobKeywords:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.keywords_df = pd.DataFrame(columns=['job', 'keywords', '자소서갯수'])
+
+    # 데이터 불러오고 기업별 자소서 갯수가 10개 미만인 기업 제외
+    def load_data(self):
+        self.df = pd.read_csv(self.data_path)
+        counts = self.df['job'].value_counts()
+        under10 = counts[counts<10].index
+        self.df = self.df.drop(self.df[self.df['job'].isin(under10)].index)
+
+    # 기업별로 자기소개서 그룹화
+    def group_data(self):
+        self.grouped_df = self.df.groupby('job')['answer'].apply(' '.join).reset_index()
+        self.content_counts = self.df['job'].value_counts().reset_index()
+        self.content_counts.columns = ['job', '자소서갯수']
+
+    # Mecab을 이용해 일반명사 추출
+    def extract_nouns(self, text):
+        mecab = Mecab()
+        tagged = mecab.pos(text)
+        return ' '.join([word for word, pos in tagged if pos == 'NNG'])
+
+    # tf-idf을 계산하여 상위 20개의 키워드 추출
+    def calculate_tfidf(self):
+        self.grouped_df['nouns'] = self.grouped_df['answer'].apply(self.extract_nouns)
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(self.grouped_df['nouns'])
+        feature_names = vectorizer.get_feature_names_out()
+
+        for idx, row in self.grouped_df.iterrows():
+            job = row['job']
+            tfidf_scores = tfidf_matrix[idx].toarray().flatten().tolist()
+            top_20_indices = sorted(range(len(tfidf_scores)), key=lambda i: tfidf_scores[i], reverse=True)[:20]
+            top_20_words = [feature_names[i] for i in top_20_indices]
+            top_20_keywords = ', '.join(top_20_words)
+            content_count = self.content_counts.loc[self.content_counts['job'] == job, '자소서갯수'].values[0]
+            self.keywords_df = self.keywords_df.append({'job': job, 'keywords': top_20_keywords, '자소서갯수': content_count}, ignore_index=True)
+
+    # 데이터 저장
+    def save_keywords(self, output_path):
+        self.keywords_df.to_csv(output_path, index=False, encoding='UTF-8')
+
+    # 통합
+    def run(self, output_path):
+        self.load_data()
+        self.group_data()
+        self.calculate_tfidf()
+        self.save_keywords(output_path)
+
+if __name__ == "__main__":
+    extractor = JobKeywords('csv/jobkorea_question_labeled_mecab.csv')
+    extractor.run('job_keywords_top20.csv')
---
+++ 4_1 input데이터 요약.py
@@ -0,0 +1,29 @@
+from gensim.summarization import summarize
+
+class TextSummary:
+    def __init__(self, input_text):
+        self.input_text = input_text
+
+    # gensim 문장 갯수
+    def sentence_count_gensim(self, text):
+        from gensim.summarization.textcleaner import split_sentences
+        return len(split_sentences(text))
+
+    def summarize_text(self):
+        # 문장 갯수가 5개 미만이면 요약하지 않음
+        sentence_count = self.sentence_count_gensim(self.input_text)
+        if sentence_count < 5:
+            return "입력한 텍스트의 문장 수가 5개 미만이므로 요약하지 않습니다."
+        
+        # 답변 요약 및 반환
+        summary = summarize(self.input_text, word_count=100)
+        if summary == "":
+            return "요약 결과가 없습니다."
+        else:
+            return summary
+
+if __name__ == "__main__":
+    input_text = input("문자열을 입력하세요: ")
+    text_summary = TextSummary(input_text)
+    summarized_text = text_summary.summarize_text()
+    print("요약 결과:\n", summarized_text)
